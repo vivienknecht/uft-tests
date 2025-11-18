6d66ca4f08c548f1ae8008c75cbd667a8df6a9eb
vknecht <vknecht@opentext.com>
2025-11-18 13:38:05 +0200

    new dist

--- dist/index.js
+++ dist/index.js
@@ -40247,7 +40247,7 @@ class Discovery {
                 }
                 LOGGER.error("This is the package name " + (0, utils_1.stripLast)(test.packageName));
                 const possibleRename = existingTests.find(et => et.packageName && et.name !== test.name);
-                if (possibleRename) {
+                if (possibleRename && !currentByName.has(possibleRename.name)) {
                     renamedTests.push({ old: possibleRename, new: test });
                     continue;
                 }
