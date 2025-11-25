1f871dd375db9450d16973f7f19abc80dd7e09b4
vknecht <vknecht@opentext.com>
2025-11-25 11:10:26 +0200

    commit

--- dist/index.js
+++ dist/index.js
@@ -30833,8 +30833,7 @@ const main = () => __awaiter(void 0, void 0, void 0, function* () {
         const sharedSpace = args.sharedSpace;
         const workspace = args.workspace;
         const clientId = args.clientId;
-        //const clientSecret = args.clientSecret;
-        const clientSecret = process.env.CLIENTSECRET || args.clientSecret;
+        const clientSecret = args.clientSecret;
         if (actionType === "convertTests") {
             convertTests();
         }
