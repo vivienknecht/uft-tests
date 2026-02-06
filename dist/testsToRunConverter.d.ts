d623cb9883be901b3ce0bebb9e98d330a26bf578
vknecht <vknecht@opentext.com>
2026-02-06 16:28:13 +0200

    testing

--- dist/testsToRunConverter.d.ts
+++ dist/testsToRunConverter.d.ts
@@ -1,3 +1,4 @@
 import Test from "./dto/Test";
 declare const convertTestsToRun: (testsToRun: Test[], framework: string, rootDirectory: string, customFramework?: string) => string;
 export default convertTestsToRun;
+//# sourceMappingURL=testsToRunConverter.d.ts.map
